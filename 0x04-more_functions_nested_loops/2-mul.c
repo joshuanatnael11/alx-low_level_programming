1a507c2ea36a1dc9987791a3dffaf4cea80c44c3
joshuanatnael11 <jojotown63372252@gmail.com>
2023-07-20 03:37:41 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first input
+ * @b: second input
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
